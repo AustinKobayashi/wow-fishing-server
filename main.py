e8ce2f3de975ba7df2fae3dca39303b62a88d52a
Austiin <austinkobayashi@gmail.com>
2023-10-18 02:52:07 -0700

    Added server

---
+++ main.py
@@ -0,0 +1,27 @@
+import pigpio
+from flask import Flask, request
+
+pi = pigpio.pi()
+
+SERVO_PIN = 27
+SERVO_SPEED = 0.1
+
+
+app = Flask(__name__)
+
+
+@app.route('/click', methods=['POST'])
+def click_handler():
+    try:
+        data = request.form
+        click = data.get('click')
+
+        pi.set_servo_pulsewidth(SERVO_PIN, click)
+
+        return "Success", 200
+    except Exception as e:
+        print(f"Error handling POST request: {e}")
+        return "Error", 500  # Internal Server Error
+
+if __name__ == '__main__':
+    app.run(debug=True)
